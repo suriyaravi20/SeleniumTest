54fc23aa3c355067331c5ed1cca1548206e21bd2
suriyaravi20 <suriyaravi20@gmail.com>
2023-05-13 13:53:33 +0530

    new comments added in AutoSelectinExamples

--- seleniumTest/src/advanceSelenium/AutoCompletionExample.java
+++ seleniumTest/src/advanceSelenium/AutoCompletionExample.java
@@ -18,29 +18,29 @@ public class AutoCompletionExample {
 		driver.get("https://letcode.in/elements");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-		
+
 		WebElement name=driver.findElement(By.name("username"));
 		name.sendKeys("suriyaravi20");
 		WebElement search=driver.findElement(By.id("search"));
 		search.click();
-		
+
 		List<WebElement> gitlis= driver.findElements(By.xpath("/html/body/app-root/app-githome/section[1]/div/div/div[1]/div/div/div/app-gitrepos/div/div/ol/li"));
 
 		int tot=gitlis.size();
 		System.out.println(tot);
-		
+
 		for (WebElement webElement : gitlis) {
 			if(webElement.getText().equals("https://github.com/suriyaravi20/SeleniumTest"))
 			{webElement.click();
 			System.out.println("selected"+webElement.getText());
 			break;
-				
+
 			}
-			
+
 		}
-		
-//		Actions act=new Actions(driver);
-//		act.keyDown(Keys.CONTROL).click(gitlis.get(3)).click().build().perform();
+		// This is another method	
+		//		Actions act=new Actions(driver);
+		//		act.keyDown(Keys.CONTROL).click(gitlis.get(3)).click().build().perform();
 	}
 
 }
